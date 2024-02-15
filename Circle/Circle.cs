93755bf626a60bc66f5b206a8ec72173a6529bc0
vladspiridonov <vlad777.96@mail.ru>
2024-02-15 23:54:21 +0300

    Добавьте файлы проекта.

---
+++ Circle/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    static class Circle
+    {
+        static public double GetLenght(double r)
+        {
+            return Math.PI * 2 * r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static public string IsInArea(double x, double y, double x0, double y0, double r)
+        {
+            double diffx = Math.Abs(x0 - x);
+            double diffy = Math.Abs(y0 - y);
+            if (diffx > r || diffy > r)
+                return "Не принадлежит";
+            double r0 = Math.Sqrt((x - x0) * (x - x0) + (y - y0) * (y - y0));
+            if (r0 > r)
+                return "Не принадлежит";
+            else
+                return "Принадлежит";
+        }
+    }
+}
---
+++ Circle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("x0:");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("y0:");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("r:");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Circle.IsInArea(x,y,x0,y0,r));
+            double l = Circle.GetLenght(r);
+            double s = Circle.GetSquare(r);
+            Console.WriteLine("Длина окружности: {0}\nПлощадь: {1}",l,s);
+            Console.ReadKey();
+        }
+    }
+}
